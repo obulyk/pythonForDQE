651d0d2665b80214d6f04066dd4ec81918fe7b7e
Obulyk <bulyk.oksana@gmail.com>
2025-03-13 20:06:26 +0200

    second task

--- list_script.py
+++ list_script.py
@@ -2,49 +2,43 @@
 import random
 
 # create empty List
-randomList100 = []
+rand_list_100 = []
 # cycle for that creates 0 to 100 cells
 for i in range(0, 100):
-    # number assign random number in diapason from0 to 1000
-    number = random.randint(0, 1000)
-    # append function adds created random number to our list
-    randomList100.append(number)
+    # numb assign random numb in diapason from0 to 1000
+    numb = random.randint(0, 1000)
+    # append function adds created random numb to our list
+    rand_list_100.append(numb)
 
-sortedListMinToMax = []
-while randomList100:
-    # first number is lowest
-    minimum = randomList100[0]
-    # cycle for x=1 to randomList1000
-    for x in randomList100:
-        # if x=1 is lower than the first number of random number
-        if x < minimum:
-            # minimum is set to X
-            minimum = x
-    # add that lower number as first number to new List
-    sortedListMinToMax.append(minimum)
-    # remove that number from list
-    randomList100.remove(minimum)
-# empty list for even Numbers
-evenNumbers = []
-# empty list for odd Numbers
-oddNumbers = []
+srt_min_2_max = []
+while rand_list_100:
+    # first numb is lowest
+    min = rand_list_100[0]
+    # cycle for x=1 to rand_list_1000
+    for x in rand_list_100:
+        # if x=1 is lower than the first numb of random numb
+        if x < min:
+            # min is set to X
+            min = x
+    # add that lower numb as first numb to new List
+    srt_min_2_max.append(min)
+    # remove that numb from list
+    rand_list_100.remove(min)
+# empty list for even numbs
+e_numb = []
+# empty list for odd numbs
+o_numb = []
 # Cycle for
-for e in sortedListMinToMax:
-    # if a number is divided on 2 without any decimal numbers
+for e in srt_min_2_max:
+    # if a numb is divided on 2 without any decimal numbs
     if (e % 2) == 0:
-        # Add to empty list with that even Numbers
-        evenNumbers.append(e)
+        # Add to empty list with that even numbs
+        e_numb.append(e)
     # another case: not even
     else:
-        # add that number to odd list
-        oddNumbers.append(e)
-    # case when we have 0, which is not even and not add
-sumaEvenNumbers = sum(evenNumbers)
-countEvenNumbers = len(evenNumbers)
-print("Average sequence fer even numbers: "
-      + str(sumaEvenNumbers / countEvenNumbers))
-sumaOddNumber = sum(oddNumbers)
-countOddNumbers = len(oddNumbers)
-# print the result
-print("Average sequence fer not even numbers: "
-      + str(sumaOddNumber / countOddNumbers))
+        # add that numb to odd list
+        o_numb.append(e)
+print("Average sequence fer even numbs: "
+      + str(sum(e_numb) / len(e_numb)))
+print("Average sequence fer not even numbs: "
+      + str(sum(o_numb) / len(o_numb)))
