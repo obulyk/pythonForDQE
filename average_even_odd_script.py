28f790f0f1951030a52f5d346fa0bbb7e2b9db6b
Obulyk <bulyk.oksana@gmail.com>
2025-03-15 15:41:04 +0200

    Comrehension for cycles are used without errors

--- average_even_odd_script.py
+++ average_even_odd_script.py
@@ -16,8 +16,8 @@ for i in range(1, len(random_numb_list)):  # Start from the second element
         last_elem = last_elem - 1
     random_numb_list[last_elem + 1] = key
 # comprehension in creating a list even numbers
-even_numbers = [ num for num in random_numb_list if num % 2 == 0]
-odd_numbers = [ num for num in random_numb_list if num % 2 != 0]
+even_numbers = [num for num in random_numb_list if num % 2 == 0]
+odd_numbers = [num for num in random_numb_list if num % 2 != 0]
 
 print("Average result for even numbers: ", sum(even_numbers)/len(even_numbers))
 print("Average result for odd numbers: ", sum(odd_numbers)/len(odd_numbers))
