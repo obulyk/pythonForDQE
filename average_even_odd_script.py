ee0bdeb96ab6cac9b4d0bc3ac5d816a99f8ef059
Obulyk <bulyk.oksana@gmail.com>
2025-03-15 15:28:15 +0200

    second task

--- average_even_odd_script.py
+++ average_even_odd_script.py
@@ -1,11 +1,8 @@
 import random
+# comprehension for list generation
+random_numb_list = [random.randint(0, 1000) for _ in range(100)]
 
-random_numb_list = []
-# cycle for creation a list (100) with random numb of integers from 0 to 1000
-for _ in range(100):
-    number = random.randint(0, 1000)
-    random_numb_list.append(number)
-# Cycle for sorting. I've used Insertion algorithm for sorting from min to max
+# Cvycle for sorting. I've used Insertion algorithm for sorting from min to max
 for i in range(1, len(random_numb_list)):  # Start from the second element
     key = random_numb_list[i]  # The current element to be inserted
     last_elem = i - 1  # The index of the last element
@@ -18,18 +15,9 @@ for i in range(1, len(random_numb_list)):  # Start from the second element
         random_numb_list[last_elem + 1] = random_numb_list[last_elem]
         last_elem = last_elem - 1
     random_numb_list[last_elem + 1] = key
-
-even_numbers = []
-# cycle for finding and adding the same list even numbers
-for num in random_numb_list:
-    if num % 2 == 0:
-        even_numbers.append(num)
-
-odd_numbers = []
-# cycle for finding and adding the same list odd numbers
-for num in random_numb_list:
-    if num % 2 != 0:
-        odd_numbers.append(num)
+# comprehension in creating a list even numbers
+even_numbers = [ num for num in random_numb_list if num % 2 == 0]
+odd_numbers = [ num for num in random_numb_list if num % 2 != 0]
 
 print("Average result for even numbers: ", sum(even_numbers)/len(even_numbers))
 print("Average result for odd numbers: ", sum(odd_numbers)/len(odd_numbers))
