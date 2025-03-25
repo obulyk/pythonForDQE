99921f0e9ee2d96b1d560c4951a978373dec54ab
Obulyk <bulyk.oksana@gmail.com>
2025-03-25 23:46:31 +0200

    new string_task in string_branch

--- string_task.py
+++ string_task.py
@@ -1,6 +1,6 @@
 import re
 
-text = """homEwork:
+homework_text = """homEwork:
   tHis iz your homeWork, copy these Text to variable.
 
 
@@ -13,33 +13,18 @@ text = """homEwork:
 
 
 
-  last iz TO calculate nuMber OF Whitespace characteRS in this Tex. caREFULL, not only Spaces, but ALL whitespaces. I got 87.
-"""
-paragraph = [sentence.strip().capitalize() for sentence in text.split('  ')]
-normalized_text = '. '.join(paragraph)
+  last iz TO calculate nuMber OF Whitespace characteRS in this Tex. caREFULL, not only Spaces, but ALL whitespaces. I got 87."""
+paragraph = [sentence.strip().capitalize() for sentence in homework_text.split('  ')]
+normalized_text = ' '.join(paragraph)
 
-normalized_text = re.sub(r'(\n\s*)', '\n', normalized_text)
+normalized_text = re.sub(r'(^|(?<=\.\s))([a-z])', lambda m: m.group(1) + m.group(2).upper(), normalized_text)
+last_words = re.findall(r'(\b\w+\b)[.?!:]', normalized_text)
+
+new_sentence = " ".join(last_words) + "."
+text = re.sub(r'(paragraph.)', r'\1 ' + new_sentence, normalized_text)
+normalized_text = re.sub(r'(?<!Fix“)iz(?!”)', 'is', text)
+
+whitespace_count = len(re.findall(r'\s', text))
 
 
-print(paragraph[2].capitalize())
-# # 1. Нормалізація: Перша літера кожного речення велика
-# normalized_text = '. '.join([sentence.strip().capitalize() for sentence in homework_text.split('. ')])
-# normalized_text = re.sub(r'(\n\s*)', '\n', normalized_text)  # Відновлюємо нові рядки
-#
-# # 2. Замінити "iZ" на "is", тільки коли це помилка
-# normalized_text = re.sub(r'\b(iZ)\b', 'is', normalized_text)
-#
-# # 3. Створити нове речення з останніх слів кожного існуючого речення
-# sentences = normalized_text.split('. ')
-# last_words = [sentence.split()[-1] for sentence in sentences if sentence]  # Останнє слово кожного речення
-# new_sentence = ' '.join(last_words) + '.'  # Створення нового речення з останніх слів
-#
-# # Додаємо нове речення до кінця
-# normalized_text += ' ' + new_sentence
-#
-# # 4. Порахувати кількість пробілів і всіх пробільних символів (whitespace)
-# whitespace_count = len(re.findall(r'\s', normalized_text))
-#
-# # Виведення результату
-# print(normalized_text)
-# print(f"Total number of whitespace characters: {whitespace_count}")
+print(normalized_text, whitespace_count)
