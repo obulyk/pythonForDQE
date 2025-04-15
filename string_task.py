640d50187c6dfe0e780993083b4ae6a2d4b5e86c
Obulyk <bulyk.oksana@gmail.com>
2025-04-15 21:37:43 +0300

    Made collection task in functions

--- string_task.py
+++ string_task.py
@@ -1,35 +1,31 @@
 import re
-import string
+
 homework_text = """homEwork:
-  tHis iz your homeWork, copy these Text to variable.
+    tHis iz your homeWork, copy these Text to variable.
 
+    You NEED TO normalize it fROM letter CASEs point oF View. also, create one MORE senTENCE witH LAST WoRDS of each existING SENtence and add it to the END OF this Paragraph.
 
+    it iZ misspeLLing here. fix“iZ” with correct “is”, but ONLY when it Iz a mistAKE.
 
-  You NEED TO normalize it fROM letter CASEs point oF View. also, create one MORE senTENCE witH LAST WoRDS of each existING SENtence and add it to the END OF this Paragraph.
+    last iz TO calculate nuMber OF Whitespace characteRS in this Tex. caREFULL, not only Spaces, but ALL whitespaces. I got 87."""
 
-
-
-  it iZ misspeLLing here. fix“iZ” with correct “is”, but ONLY when it Iz a mistAKE.
-
-
-
-  last iz TO calculate nuMber OF Whitespace characteRS in this Tex. caREFULL, not only Spaces, but ALL whitespaces. I got 87."""
 
 paragraph = [sentence.strip().capitalize()
-             for sentence in homework_text.split(b"\xc2\xa0".decode("utf-8"))]
+             for sentence in homework_text.split(' ' and '\n')]
 normalized_text = '\n'.join(paragraph)
 
 normalized_text = re.sub(r'(^|(?<=\.\s))([a-z])',
                          lambda m: m.group(1) + m.group(2).upper(),
                          normalized_text)
-
 last_words = re.findall(r'(\b\w+\b)[.?!:]', normalized_text)
 
 new_sentence = " ".join(last_words) + "."
 text = re.sub(r'(paragraph.)', r'\1 ' + new_sentence, normalized_text)
+normalized_text = re.sub(r' iz ', ' is ', text)
 
-normalized_text = re.sub(r'(?<!Fix“)iz(?!”)', 'is', text)
+normalized_text = normalized_text.replace('\t',' ')
 
-whitespace = len([i for i in normalized_text if i in string.whitespace])
-#whitespace_count = len(re.findall(r'[\s\xa0]', normalized_text))
-print(normalized_text, '\n', whitespace)
+whitespace_count = len(re.findall(r'\s+', normalized_text))
+
+print(normalized_text, '\n', whitespace_count)
+
